2d10e30da9993e3ed18b4ea9a20fed70d96e3c03
lidevlin <liam.devlin@iqvia.com>
2022-08-25 11:00:25 +0100

    Added meta description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
+import Head from "next/head";
 import BookSection from "../components/v2/BookSection";
 import Hero from "../components/v2/Hero";
+import { Description } from "../data/description";
 
 const v2 = () => {
   return (
     <div className="h-screen w-screen">
+      <Head>
+        <title>Fleet Bookclub</title>
+        <meta name="description" content={Description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Hero />
       <BookSection />
     </div>
