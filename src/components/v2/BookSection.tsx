5b9cc3899412c51820f6f3c49bde24360e3aa755
lidevlin <lidevlin95@gmail.com>
2022-12-23 19:59:36 +0000

    Updated new book, and prepared to add ratings next year

--- src/components/v2/BookSection.tsx
+++ src/components/v2/BookSection.tsx
@@ -16,6 +16,7 @@ const BookSection = (props: Props) => {
             desc={book.Description}
             month={book.Month}
             year={book.Year}
+            rating={book.Rating}
           />
         );
       })}
