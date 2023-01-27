4d87b053bf5693ed8243bfab43bfd9c5cd0b8db8
lidevlin <lidevlin95@gmail.com>
2023-01-27 12:42:12 +0000

    Updated images in modal mode

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Card = ({ children, month, year }: Props) => {
   return (
-    <div className="col-span-1 border-gray-600 relative h-[32rem]">
+    <div className="col-span-1 border-gray-600 relative md:h-[32rem] h-[15rem]">
       <div className="absolute z-10 bg-purple-300 rounded-br-xl pr-2 pl-1 md:text-xl rounded-tl-md text-[.75rem] col-span-4 inset-top-0 inset-left-0">
         <p className="">{month + " " + year}</p>
       </div>
